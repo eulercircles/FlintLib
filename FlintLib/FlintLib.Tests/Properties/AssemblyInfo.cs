13665e1b91bd261e4370e40b1cc8227ae28af416
Eulercircles <ronaldeflint@gmail.com>
2016-01-14 10:38:59 -0600

    Initial commit.

---
+++ FlintLib/FlintLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+using FlintLib.Common;
+
+[assembly: AssemblyTitle("FlintLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FlintLib.Tests")]
+[assembly: AssemblyCopyright("Copyright © Ronald E Flint 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("44d80f3e-321d-4db3-8049-6d641b21d94c")]
+
+[assembly: AssemblyVersion(ProductVersion.Number + ".1.*")]
