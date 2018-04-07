aac4c92aea9fb6a809385231f42013dee1182587
eulercircles <RonaldEFlint@gmail.com>
2018-04-07 19:16:03 -0400

    Created a project for types and routines associated with Accounting.

---
+++ FlintLib.Accounting/IAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlintLib.Accounting
+{
+	public interface IAccount
+	{
+		double Balance { get; }
+
+		void Debit(IAccount target);
+		void Credit(IAccount source);
+	}
+}
