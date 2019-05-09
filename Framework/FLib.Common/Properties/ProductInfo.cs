d2ae87f5a32d1dc5e42a27a5a123b7f2e9f956eb
Ronald Flint <ronaldeflint@gmail.com>
2019-05-09 18:20:01 -0400

    Updated all projects to generate XML documentation when building in the Release configuration.

--- Framework/FLib.Common/Properties/ProductInfo.cs
+++ Framework/FLib.Common/Properties/ProductInfo.cs
@@ -2,15 +2,18 @@
 
 namespace FLib.Common.Properties
 {
+	/// <summary>
+	/// Shared product info.
+	/// </summary>
 	public static class ProductInfo
 	{
-
+		/// <summary>Major and minor version.</summary>
 		public const string VersionMajorMinor = "2019.5.";
 
-
+		/// <summary>Description</summary>
 		public const string Description = "Development Pre-Release";
 
-
+		/// <summary>Copyright Info</summary>
 		public const string Copyright = "Copyright © 2015-2019 Ronald E Flint";
 	}
 }
